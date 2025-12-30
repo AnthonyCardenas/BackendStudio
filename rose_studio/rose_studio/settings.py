f820e4721de3c44114f85ecc839f1a1a2f5dfc6c
Anthony Cardenas <ajcardenas003@gmail.com>
2025-12-30 17:48:20 -0600

    added host permission

--- rose_studio/rose_studio/settings.py
+++ rose_studio/rose_studio/settings.py
@@ -31,6 +31,9 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
+    "www.brooklynnrosestudio.com",
+    "brooklynnrosestudio.com",
+    "18.191.152.147",
 ]
 
 CORS_ALLOWED_ORIGINS = [
